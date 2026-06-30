1373345ad37e2ba97e4d52a4d74173b452c1d170
AMINUDDIN AZNAN <2023695716@student.uitm.edu.my>
2026-06-30 15:22:55 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username === "admin" && password === "123456"){
+        window.location.href = "dashboard.html";
+    }else{
+        document.getElementById("errorMessage").innerHTML =
+        "Invalid username or password!";
+    }
+
+});
